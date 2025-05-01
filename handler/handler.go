9387a9850ac01bf1e337d82c0e26c677f3760a80
Everton Silva <everton.csilva@experian.com>
2025-05-01 16:34:53 -0300

    feat: create InitializeHandler function.

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,17 @@
 package handler
+
+import (
+	"github.com/costa-everton/goportunities/config"
+	"gorm.io/gorm"
+)
+
+
+var (
+	logger	*config.Logger
+	db		*gorm.DB
+)
+
+func InitializeHandler () {
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
+}
