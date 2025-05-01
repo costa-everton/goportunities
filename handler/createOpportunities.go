d4dbd01647192c9bc38a7c9d665551ae5d0f6740
Everton Silva <everton.csilva@experian.com>
2025-05-01 16:37:02 -0300

    feat: implement logic to create opportunities

--- handler/createOpportunities.go
+++ handler/createOpportunities.go
@@ -3,11 +3,35 @@ package handler
 import (
 	"net/http"
 
+	"github.com/costa-everton/goportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateOpportunitiesHandler(ctx *gin.Context){
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "POST opportunities create",
-	})
+	request := CreateOpportunitiesRequest{}
+
+	ctx.BindJSON(&request)
+	if err:= request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	opportunities := schemas.Opportunities{
+		Role: request.Role,
+		Company: request.Company,
+		Location: request.Location,
+		Remote: *request.Remote,
+		Link: request.Link,
+		Salary: request.Salary,
+	}
+
+	err := db.Create(&opportunities).Error
+	if err != nil {
+		logger.Errorf("error creating opportunities: %v", err.Error())
+		sendError(ctx, "error creating opportunities on database", http.StatusInternalServerError)
+		return
+	}
+
+	sendSuccess(ctx, "create-opportuninies", opportunities)
 }
