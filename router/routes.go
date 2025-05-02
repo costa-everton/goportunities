34675a21beec4fe466b5552b32e9191a8f2df468
Everton Silva <everton.csilva@experian.com>
2025-05-01 22:26:47 -0300

    feat: update function name

--- router/routes.go
+++ router/routes.go
@@ -13,9 +13,9 @@ func initializeRoutes(router *gin.Engine){
 	v1 := router.Group( "/api/v1")
 	{
 		v1.POST("/create-opportunities", handler.CreateOpportunitiesHandler)
-		v1.GET( "/opening-opportunities", handler.ShowOpportunitiesHandler)
+		v1.GET( "/show-opportunitie", handler.ShowOpportunitieHandler)
 		v1.GET( "/list-opportunities", handler.ListOpportunitiesHandler)
-		v1.PUT("/update-opportunities", handler.UpdateOpportunitiesHandler)
+		v1.PUT("/update-opportunitie", handler.UpdateOpportunitieHandler)
 		v1.DELETE("/delete-opportunities", handler.DeleteOpportunitiesHandler)
 	}
 }
